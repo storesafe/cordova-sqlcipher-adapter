1fc33ef836cd42c3d097b71d99caf8931586f000
Chris Brody <chris.brody@gmail.com>
2014-10-02 19:58:25 +0200

    Port of transactions semantics mechanism by @ef4 working with Android version in Cordova 2.7 (manual installation)

--- Android/assets/www/SQLitePlugin-orig.coffee
+++ Android/assets/www/SQLitePlugin-orig.coffee
@@ -29,6 +29,22 @@ do ->
   SQLitePlugin::databaseFeatures = isSQLitePluginDatabase: true
   SQLitePlugin::openDBs = {}
 
+  SQLitePlugin::txQ = []
+
+  SQLitePlugin::transaction = (fn, error, success) ->
+    t = new SQLitePluginTransaction(this, fn, error, success)
+    @txQ.push t
+    if @txQ.length is 1
+      t.start()
+    return
+
+  SQLitePlugin::startNextTransaction = ->
+    @txQ.shift()
+    if @txQ[0]
+      @txQ[0].start()
+    return
+
+
   # TBD old mechanism (going away):
   SQLitePlugin::batchTransaction = (fn, error, success) ->
     t = new SQLiteBatchTransaction(@dbname)
@@ -37,7 +53,7 @@ do ->
     return
 
   # TBD old mechanism (going away):
-  SQLitePlugin::transaction = SQLitePlugin::batchTransaction
+  #SQLitePlugin::transaction = SQLitePlugin::batchTransaction
 
   SQLitePlugin::open = (success, error) ->
     console.log "SQLitePlugin.prototype.open"
@@ -60,7 +76,7 @@ do ->
   #  return
 
   # TBD old mechanism (going away):
-  get_unique_id = ->
+  get_uniqid = ->
     id = new Date().getTime()
     id2 = new Date().getTime()
     id2 = new Date().getTime()  while id is id2
@@ -74,7 +90,7 @@ do ->
   SQLiteBatchTransaction = (dbname) ->
     @dbname = dbname
     @executes = []
-    @trans_id = get_unique_id()
+    @trans_id = get_uniqid()
     @__completed = false
     @__submitted = false
     # TBD obsolete and going away:
@@ -150,7 +166,7 @@ do ->
     new_query["trans_id"] = trans_id
 
     if callback or not @optimization_no_nested_callbacks
-      new_query["qid"] = get_unique_id()
+      new_query["qid"] = get_uniqid()
     else
       if @optimization_no_nested_callbacks
         new_query["qid"] = ""
@@ -244,6 +260,212 @@ do ->
     cordova.exec null, null, "SQLitePlugin", "executeBatchTransaction", [ {dbargs: {dbname: @dbname}, executes: queryQ[@trans_id]} ]
     return
 
+
+
+  uid = 1000
+
+  get_unique_id = -> ++uid
+
+  trcbq = {}
+
+  # TBD ???:
+  #batchcbmap = {}
+
+  SQLitePluginTransaction = (db, fn, error, success) ->
+    @trid = get_unique_id()
+    trcbq[@trid] = {}
+    if typeof(fn) != "function"
+      # This is consistent with the implementation in Chrome -- it
+      # throws if you pass anything other than a function. This also
+      # prevents us from stalling our txQueue if somebody passes a
+      # false value for fn.
+      throw new Error("transaction expected a function")
+    @db = db
+    @fn = fn
+    @error = error
+    @success = success
+    @executes = []
+    @executeSql "BEGIN", [], null, (tx, err) ->
+      throw new Error("unable to begin transaction: " + err.message)
+    return
+
+  SQLiteTransactionCB =
+    batchCompleteCallback: (cbResult) ->
+      console.log "SQLiteTransactionCB.batchCompleteCallback cbResult #{JSON.stringify cbResult}"
+
+      trid = cbResult.trid
+      result = cbResult.result
+
+      for r in result
+        type = r.type
+        qid = r.qid
+        res = r.result
+
+        t = trcbq[trid]
+
+        if t
+          q = t[qid]
+
+          if q
+            if q[type]
+              q[type] res
+
+            # ???:
+            delete trcbq[trid][qid]
+
+      return
+
+  SQLitePluginTransaction::start = ->
+    try
+      return  unless @fn
+      @fn this
+      @fn = null
+      @run()
+    catch err
+      # If "fn" throws, we must report the whole transaction as failed.
+      @db.startNextTransaction()
+      if @error
+        @error err
+    return
+
+  SQLitePluginTransaction::executeSql = (sql, values, success, error) ->
+    #console.log "SQLitePluginTransaction::executeSql"
+    qid = get_unique_id()
+
+    @executes.push
+      #query: [sql].concat(values or [])
+      success: success
+      error: error
+      qid: qid
+
+      sql: sql
+      params: values
+
+    return
+
+  SQLitePluginTransaction::handleStatementSuccess = (handler, response) ->
+    if !handler
+      return
+
+    rows = response.rows || []
+    payload =
+      rows:
+        item: (i) ->
+          rows[i]
+
+        length: rows.length
+
+      rowsAffected: response.rowsAffected or 0
+      insertId: response.insertId or undefined
+
+    handler this, payload
+
+    return
+
+  SQLitePluginTransaction::handleStatementFailure = (handler, response) ->
+    if !handler
+      throw new Error "a statement with no error handler failed: " + response.message
+    if handler(this, response)
+      throw new Error "a statement error callback did not return false"
+    return
+
+  SQLitePluginTransaction::run = ->
+    #console.log "SQLitePluginTransaction::run"
+    txFailure = null
+
+    tropts = []
+    batchExecutes = @executes
+    waiting = batchExecutes.length
+    @executes = []
+    tx = this
+
+    # TBD ???:
+    #batchid = get_unique_id()
+
+    handlerFor = (index, didSucceed) ->
+      (response) ->
+        try
+          if didSucceed
+            tx.handleStatementSuccess batchExecutes[index].success, response
+          else
+            tx.handleStatementFailure batchExecutes[index].error, response
+        catch err
+          txFailure = err  unless txFailure
+
+        if --waiting == 0
+          if txFailure
+            tx.rollBack txFailure
+          else if tx.executes.length > 0
+            # new requests have been issued by the callback
+            # handlers, so run another batch.
+            tx.run()
+          else
+            tx.commit()
+
+    i = 0
+
+    while i < batchExecutes.length
+      request = batchExecutes[i]
+
+      qid = request.qid
+
+      trcbq[@trid][qid] =
+        success: handlerFor(i, true)
+        error: handlerFor(i, false)
+
+      tropts.push
+        qid: qid
+        # for ios version:
+        query: [request.sql].concat(request.params)
+        # XXX trans_id going away:
+        trans_id: @trid
+        sql: request.sql
+        params: request.params || []
+
+      i++
+
+    # XXX TODO fix command Android/iOS/WP(8):
+    cordova.exec null, null, "SQLitePlugin", "executeBatchTransaction", [ {dbargs: {dbname: @db.dbname}, executes: tropts} ]
+    return
+
+  SQLitePluginTransaction::rollBack = (txFailure) ->
+    if @finalized then return
+    tx = @
+
+    succeeded = ->
+      delete trcbq[@trid]
+      tx.db.startNextTransaction()
+      if tx.error then tx.error txFailure
+
+    failed = (tx, err) ->
+      delete trcbq[@trid]
+      tx.db.startNextTransaction()
+      if tx.error then tx.error new Error("error while trying to roll back: " + err.message)
+
+    @finalized = true
+    @executeSql "ROLLBACK", [], succeeded, failed
+    @run()
+    return
+
+  SQLitePluginTransaction::commit = ->
+    if @finalized then return
+    tx = @
+
+    succeeded = ->
+      delete trcbq[@trid]
+      tx.db.startNextTransaction()
+      if tx.success then tx.success()
+
+    failed = (tx, err) ->
+      delete trcbq[@trid]
+      tx.db.startNextTransaction()
+      if tx.error then tx.error new Error("error while trying to commit: " + err.message)
+
+    @finalized = true
+    @executeSql "COMMIT", [], succeeded, failed
+    @run()
+    return
+
   SQLiteFactory =
     # NOTE: this function should NOT be translated from Javascript
     # back to CoffeeScript by js2coffee.
@@ -275,7 +497,8 @@ do ->
 
   # required for callbacks:
   # TBD old mechanism (going away):
-  root.SQLiteTransactionCB = SQLiteBatchTransactionCB
+  #root.SQLiteTransactionCB = SQLiteBatchTransactionCB
+  root.SQLiteTransactionCB = SQLiteTransactionCB
 
   root.sqlitePlugin =
     sqliteFeatures:
--- Android/assets/www/SQLitePlugin.js
+++ Android/assets/www/SQLitePlugin.js
@@ -1,5 +1,5 @@
 (function() {
-  var SQLiteBatchTransaction, SQLiteBatchTransactionCB, SQLiteFactory, SQLitePlugin, SQLiteQueryCB, get_unique_id, queryCBQ, queryQ, root;
+  var SQLiteBatchTransaction, SQLiteBatchTransactionCB, SQLiteFactory, SQLitePlugin, SQLitePluginTransaction, SQLiteQueryCB, SQLiteTransactionCB, get_uniqid, get_unique_id, queryCBQ, queryQ, root, trcbq, uid;
   root = this;
   SQLitePlugin = function(openargs, openSuccess, openError) {
     var dbname;
@@ -24,13 +24,27 @@
     isSQLitePluginDatabase: true
   };
   SQLitePlugin.prototype.openDBs = {};
+  SQLitePlugin.prototype.txQ = [];
+  SQLitePlugin.prototype.transaction = function(fn, error, success) {
+    var t;
+    t = new SQLitePluginTransaction(this, fn, error, success);
+    this.txQ.push(t);
+    if (this.txQ.length === 1) {
+      t.start();
+    }
+  };
+  SQLitePlugin.prototype.startNextTransaction = function() {
+    this.txQ.shift();
+    if (this.txQ[0]) {
+      this.txQ[0].start();
+    }
+  };
   SQLitePlugin.prototype.batchTransaction = function(fn, error, success) {
     var t;
     t = new SQLiteBatchTransaction(this.dbname);
     fn(t);
     t.complete(success, error);
   };
-  SQLitePlugin.prototype.transaction = SQLitePlugin.prototype.batchTransaction;
   SQLitePlugin.prototype.open = function(success, error) {
     console.log("SQLitePlugin.prototype.open");
     if (!(this.dbname in this.openDBs)) {
@@ -38,7 +52,7 @@
       cordova.exec(success, error, "SQLitePlugin", "open", [this.openargs]);
     }
   };
-  get_unique_id = function() {
+  get_uniqid = function() {
     var id, id2;
     id = new Date().getTime();
     id2 = new Date().getTime();
@@ -52,7 +66,7 @@
   SQLiteBatchTransaction = function(dbname) {
     this.dbname = dbname;
     this.executes = [];
-    this.trans_id = get_unique_id();
+    this.trans_id = get_uniqid();
     this.__completed = false;
     this.__submitted = false;
     this.optimization_no_nested_callbacks = true;
@@ -140,7 +154,7 @@
     new_query = new Object();
     new_query["trans_id"] = trans_id;
     if (callback || !this.optimization_no_nested_callbacks) {
-      new_query["qid"] = get_unique_id();
+      new_query["qid"] = get_uniqid();
     } else {
       if (this.optimization_no_nested_callbacks) {
         new_query["qid"] = "";
@@ -249,6 +263,212 @@
       }
     ]);
   };
+  uid = 1000;
+  get_unique_id = function() {
+    return ++uid;
+  };
+  trcbq = {};
+  SQLitePluginTransaction = function(db, fn, error, success) {
+    this.trid = get_unique_id();
+    trcbq[this.trid] = {};
+    if (typeof fn !== "function") {
+      throw new Error("transaction expected a function");
+    }
+    this.db = db;
+    this.fn = fn;
+    this.error = error;
+    this.success = success;
+    this.executes = [];
+    this.executeSql("BEGIN", [], null, function(tx, err) {
+      throw new Error("unable to begin transaction: " + err.message);
+    });
+  };
+  SQLiteTransactionCB = {
+    batchCompleteCallback: function(cbResult) {
+      var q, qid, r, res, result, t, trid, type, _i, _len;
+      console.log("SQLiteTransactionCB.batchCompleteCallback cbResult " + (JSON.stringify(cbResult)));
+      trid = cbResult.trid;
+      result = cbResult.result;
+      for (_i = 0, _len = result.length; _i < _len; _i++) {
+        r = result[_i];
+        type = r.type;
+        qid = r.qid;
+        res = r.result;
+        t = trcbq[trid];
+        if (t) {
+          q = t[qid];
+          if (q) {
+            if (q[type]) {
+              q[type](res);
+            }
+            delete trcbq[trid][qid];
+          }
+        }
+      }
+    }
+  };
+  SQLitePluginTransaction.prototype.start = function() {
+    var err;
+    try {
+      if (!this.fn) {
+        return;
+      }
+      this.fn(this);
+      this.fn = null;
+      this.run();
+    } catch (_error) {
+      err = _error;
+      this.db.startNextTransaction();
+      if (this.error) {
+        this.error(err);
+      }
+    }
+  };
+  SQLitePluginTransaction.prototype.executeSql = function(sql, values, success, error) {
+    var qid;
+    qid = get_unique_id();
+    this.executes.push({
+      success: success,
+      error: error,
+      qid: qid,
+      sql: sql,
+      params: values
+    });
+  };
+  SQLitePluginTransaction.prototype.handleStatementSuccess = function(handler, response) {
+    var payload, rows;
+    if (!handler) {
+      return;
+    }
+    rows = response.rows || [];
+    payload = {
+      rows: {
+        item: function(i) {
+          return rows[i];
+        },
+        length: rows.length
+      },
+      rowsAffected: response.rowsAffected || 0,
+      insertId: response.insertId || void 0
+    };
+    handler(this, payload);
+  };
+  SQLitePluginTransaction.prototype.handleStatementFailure = function(handler, response) {
+    if (!handler) {
+      throw new Error("a statement with no error handler failed: " + response.message);
+    }
+    if (handler(this, response)) {
+      throw new Error("a statement error callback did not return false");
+    }
+  };
+  SQLitePluginTransaction.prototype.run = function() {
+    var batchExecutes, handlerFor, i, qid, request, tropts, tx, txFailure, waiting;
+    txFailure = null;
+    tropts = [];
+    batchExecutes = this.executes;
+    waiting = batchExecutes.length;
+    this.executes = [];
+    tx = this;
+    handlerFor = function(index, didSucceed) {
+      return function(response) {
+        var err;
+        try {
+          if (didSucceed) {
+            tx.handleStatementSuccess(batchExecutes[index].success, response);
+          } else {
+            tx.handleStatementFailure(batchExecutes[index].error, response);
+          }
+        } catch (_error) {
+          err = _error;
+          if (!txFailure) {
+            txFailure = err;
+          }
+        }
+        if (--waiting === 0) {
+          if (txFailure) {
+            return tx.rollBack(txFailure);
+          } else if (tx.executes.length > 0) {
+            return tx.run();
+          } else {
+            return tx.commit();
+          }
+        }
+      };
+    };
+    i = 0;
+    while (i < batchExecutes.length) {
+      request = batchExecutes[i];
+      qid = request.qid;
+      trcbq[this.trid][qid] = {
+        success: handlerFor(i, true),
+        error: handlerFor(i, false)
+      };
+      tropts.push({
+        qid: qid,
+        query: [request.sql].concat(request.params),
+        trans_id: this.trid,
+        sql: request.sql,
+        params: request.params || []
+      });
+      i++;
+    }
+    cordova.exec(null, null, "SQLitePlugin", "executeBatchTransaction", [
+      {
+        dbargs: {
+          dbname: this.db.dbname
+        },
+        executes: tropts
+      }
+    ]);
+  };
+  SQLitePluginTransaction.prototype.rollBack = function(txFailure) {
+    var failed, succeeded, tx;
+    if (this.finalized) {
+      return;
+    }
+    tx = this;
+    succeeded = function() {
+      delete trcbq[this.trid];
+      tx.db.startNextTransaction();
+      if (tx.error) {
+        return tx.error(txFailure);
+      }
+    };
+    failed = function(tx, err) {
+      delete trcbq[this.trid];
+      tx.db.startNextTransaction();
+      if (tx.error) {
+        return tx.error(new Error("error while trying to roll back: " + err.message));
+      }
+    };
+    this.finalized = true;
+    this.executeSql("ROLLBACK", [], succeeded, failed);
+    this.run();
+  };
+  SQLitePluginTransaction.prototype.commit = function() {
+    var failed, succeeded, tx;
+    if (this.finalized) {
+      return;
+    }
+    tx = this;
+    succeeded = function() {
+      delete trcbq[this.trid];
+      tx.db.startNextTransaction();
+      if (tx.success) {
+        return tx.success();
+      }
+    };
+    failed = function(tx, err) {
+      delete trcbq[this.trid];
+      tx.db.startNextTransaction();
+      if (tx.error) {
+        return tx.error(new Error("error while trying to commit: " + err.message));
+      }
+    };
+    this.finalized = true;
+    this.executeSql("COMMIT", [], succeeded, failed);
+    this.run();
+  };
   SQLiteFactory = {
     opendb: function() {
       var errorcb, first, okcb, openargs;
@@ -281,7 +501,7 @@
       return new SQLitePlugin(openargs, okcb, errorcb);
     }
   };
-  root.SQLiteTransactionCB = SQLiteBatchTransactionCB;
+  root.SQLiteTransactionCB = SQLiteTransactionCB;
   return root.sqlitePlugin = {
     sqliteFeatures: {
       isSQLitePlugin: true
